9a5949343142af538e4297987bc20a0022fb6ca0
Christopher Ishida <cishida@ucla.edu>
2014-08-29 19:49:03 -0700

    User can set a first and last name

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,28 @@ class UsersController < ApplicationController
 	def profile
 		@user = current_user
 	end
+
+	def edit_name
+		@user = current_user
+	end
+
+	def update_name
+		@user = current_user
+		#Update the object
+		if @user.update_attributes(user_params)
+			#If update succeeds, redirect to the index action
+			flash[:notice] = "Name updated successfully"
+			redirect_to(:action => 'profile')
+
+		else
+			#If save fails, re-display the form so user can fix errors
+			render('edit_name')
+		end
+	end
+
+	private
+
+	def user_params
+		params.require(:user).permit(:first_name, :last_name)
+	end
 end
